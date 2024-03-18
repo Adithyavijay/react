964a14915f2f146e965730ca77e79ef478140efe
Adithya vijay <adithyavijay99@gmail.com>
2024-03-18 05:36:50 +0530

    episode 2

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement('div',{id : 'parent'}, [
-          React.createElement('div',{id : 'child'}, [
+          React.createElement('div',{id : 'child1'}, [
             React.createElement('h1',{id : 'h1'},"this is h1 tag"),
             React.createElement('h2', { id :'h2'} , "this is h2 tag" ) ] ),
-            React.createElement('div',{id : 'child'}, [
+            React.createElement('div',{id : 'child2'}, [
                 React.createElement('h1',{id : 'h1'},"this is h1 tag"),
                 React.createElement('h2', { id :'h2'} , "this is h2 tag" ) ] )
              ] )
